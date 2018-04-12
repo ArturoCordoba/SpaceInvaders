c1f48c948f8e0159eae45c7b968817220f089e12
ArturoCordoba <arturocv16@gmail.com>
2018-04-12 16:20:32 -0600

    Modo observador completamente implementado

--- src/gui/ObserverScreen.java
+++ src/gui/ObserverScreen.java
@@ -1,7 +1,6 @@
 package gui;
 
 import datastructures.LinkedList;
-import entities.*;
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -17,19 +16,114 @@ import java.io.FileNotFoundException;
 
 @SuppressWarnings("Duplicates")
 public class ObserverScreen extends GameScreen{
+    private Image ovniImage;
+    private Image crabImage;
+    private Image octopusImage;
+    private Image squidImage;
+
+    private Image[] ovniFrames;
+    private Image[] crabFrames;
+    private Image[] octopusFrames;
+    private Image[] squidFrames;
+
+    private Double ovniElapsedTime = 0.0;
+    private Double crabElapsedTime = 0.0;
+    private Double octopusElapsedTime = 0.0;
+    private Double squidElapsedTime = 0.0;
+
+    private Integer ovniIndex = 0;
+    private Integer crabIndex = 0;
+    private Integer octopusIndex = 0;
+    private Integer squidIndex = 0;
+
     /**
      * Constructor
      * @param mainStage Stage principal de la aplicacion
      */
     public ObserverScreen(Stage mainStage) {
         super(mainStage, "");
-        enemyList = new LinkedList<Enemy>();
-        playerProjectileList = new LinkedList<Projectile>();
-        enemyProjectileList = new LinkedList<Projectile>();
-        blockList = new LinkedList<Block>();
+        loadFrames();
         render();
     }
 
+    /**
+     * Metodo para cargar todos los frames de las animaciones
+     */
+    private void loadFrames(){
+        //Se cargan los frames del ovni
+        ovniFrames = new Image[19];
+        for (Integer i = 1; i <= 19; i++) {
+            ovniFrames[i - 1] = new Image("resources/ovni" + i + ".png");
+        }
+
+        //Se cargan los frames del crab
+        crabFrames = new Image[2];
+        for (Integer i = 1; i <= 2; i++) {
+            crabFrames[i - 1] = new Image("resources/crab" + i + ".png");
+        }
+
+        //Se cargan los frames del octopus
+        octopusFrames = new Image[2];
+        for (Integer i = 1; i <= 2; i++) {
+            octopusFrames[i - 1] = new Image("resources/octopus" + i + ".png");
+        }
+
+        //Se cargan los frames del squid
+        squidFrames = new Image[2];
+        for (Integer i = 1; i <= 2; i++) {
+            squidFrames[i - 1] = new Image("resources/squid" + i + ".png");
+        }
+    }
+
+    /**
+     * Metodo para actualizar el Image de cada animacion
+     * @param time Tiempo transcurrido desde el ultimo frame
+     */
+    private void updateFrames(Double time){
+        //Animacion del ovni
+        ovniElapsedTime += time;
+        if(ovniElapsedTime >= 0.02) {
+            ovniElapsedTime = 0.0;
+            ovniIndex += 1;
+            if(ovniIndex >= ovniFrames.length)
+                ovniIndex = 0;
+        }
+        ovniImage = ovniFrames[ovniIndex];
+
+        //Animacion del crab
+        crabElapsedTime += time;
+        if(crabElapsedTime >= 0.5) {
+            crabElapsedTime = 0.0;
+            crabIndex += 1;
+            if(crabIndex >= crabFrames.length)
+                crabIndex = 0;
+        }
+        crabImage = crabFrames[crabIndex];
+
+        //Animacion del octopus
+        octopusElapsedTime += time;
+        if(octopusElapsedTime >= 0.5) {
+            octopusElapsedTime = 0.0;
+            octopusIndex += 1;
+            if(octopusIndex >= octopusFrames.length)
+                octopusIndex = 0;
+        }
+        octopusImage = octopusFrames[octopusIndex];
+
+        //Animacion del squid
+        squidElapsedTime += time;
+        if(squidElapsedTime >= 0.5) {
+            squidElapsedTime = 0.0;
+            squidIndex += 1;
+            if(squidIndex >= squidFrames.length)
+                squidIndex = 0;
+        }
+        squidImage = squidFrames[squidIndex];
+    }
+
+    /**
+     * Metodo para renderizar el contenido enviado por el servidor
+     */
     @Override
     protected void render(){
         Canvas canvas = new Canvas(width, height);
@@ -47,48 +141,60 @@ public class ObserverScreen extends GameScreen{
         gcontext.setFont(font);
         gcontext.setFill(Color.WHITE);
 
+        //Se cargan las imagenes de los corazones, el jugador, las balas, y los bloques
         Image heartImage = new Image("resources/heart.png");
-        Image ovniImage = new Image("resources/ovni7.png");
-        Image crabImage = new Image("resources/crab1.png");
-        Image octopusImage = new Image("resources/octopus1.png");
-        Image squidImage = new Image("resources/squid1.png");
         Image playerImage = new Image("resources/canon.png");
         Image playerBulletImage = new Image("resources/laser_blue.png");
         Image enemyBulletImage = new Image("resources/laser_red.png");
         Image blockImage = new Image("resources/block.png");
 
+        lastNanoTime = new Long(System.nanoTime());
+
         new AnimationTimer(){
 
             @Override
-            public void handle(long l) {
+            public void handle(long currentNanoTime) {
+                Double elapsedTime = (currentNanoTime - lastNanoTime) / 1000000000.0;
+                lastNanoTime = currentNanoTime;
+
+                updateFrames(elapsedTime);
+
+                //Se limpia el graphic context
                 gcontext.clearRect(0, 0, width, height);
 
+                //Se recibe el string proveniente del servidor
                 String info = Main.getSocket().receiveString();
 
+                //Se dividen los distintos elementos recibidos
                 LinkedList<String> infoList = SplitString.split(info, "/");
 
+                //Se obtiene el nivel
                 String levelS = (String) infoList.getElement(5).getDataT();
                 gcontext.fillText(levelS, 110, 75); //level
 
+                //Se obtienen las diferentes listas de entidades
                 LinkedList<String> enemyList = SplitString.split((String)infoList.getElement(4).getDataT(), "_");
                 LinkedList<String> player = SplitString.split((String)infoList.getElement(3).getDataT(), "_");
                 LinkedList<String> enemyProjectileList = SplitString.split((String)infoList.getElement(2).getDataT(), "_");
                 LinkedList<String> playerProjectileList = SplitString.split((String)infoList.getElement(1).getDataT(), "_");
                 LinkedList<String> blockList = SplitString.split((String)infoList.getElement(0).getDataT(), "_");
 
-                extractEnemies(enemyList, crabImage, squidImage, octopusImage, ovniImage, gcontext);
-
+                //Se renderizan las entidades obtenidas
+                extractEnemies(enemyList, gcontext);
                 extractPlayer(player, gcontext, playerImage, heartImage);
-
                 extractProjectiles(enemyProjectileList, playerProjectileList, enemyBulletImage, playerBulletImage, gcontext);
-
                 extractBlocks(blockList, blockImage, gcontext);
 
             }
         }.start();
     }
 
-    private void extractEnemies(LinkedList<String> enemyList, Image crab, Image squid, Image octopus, Image ovni, GraphicsContext gcontext){
+    /**
+     * Metodo para extraer los enemigos del string recibido
+     * @param enemyList Lista con los string de los enemigos
+     * @param gcontext GraphicsContext en el que se dibuja
+     */
+    private void extractEnemies(LinkedList<String> enemyList, GraphicsContext gcontext){
         for (Integer i = 0; i < enemyList.getSize(); i++) {
             LinkedList<String> enemy = SplitString.split((String) enemyList.getElement(i).getDataT(), ",");
 
@@ -97,19 +203,26 @@ public class ObserverScreen extends GameScreen{
             Double y = Double.valueOf((String) enemy.getElement(2).getDataT());
 
             if(((String)enemy.getElement(5).getDataT()).compareTo("Ovni") == 0){
-                image = ovni;
+                image = ovniImage;
             } else if(((String)enemy.getElement(5).getDataT()).compareTo("Crab") == 0){
-                image = crab;
+                image = crabImage;
             } else if(((String)enemy.getElement(5).getDataT()).compareTo("Octopus") == 0){
-                image = octopus;
+                image = octopusImage;
             } else {
-                image = squid;
+                image = squidImage;
             }
 
             gcontext.drawImage(image, x, y);
         }
     }
 
+    /**
+     * Metodo para extraer la informacion del jugador
+     * @param playerList Informacion de jugador
+     * @param gcontext GraphicsContext en el que se dibuja
+     * @param playerImage Imagen de la nave del jugador
+     * @param heartImage Imagen de los corazones de vida
+     */
     private void extractPlayer(LinkedList<String> playerList, GraphicsContext gcontext, Image playerImage, Image heartImage){
         LinkedList<String> player = SplitString.split((String) playerList.getElement(0).getDataT(), ",");
 
@@ -132,6 +245,14 @@ public class ObserverScreen extends GameScreen{
         gcontext.fillText(score, 110, 50); //score
     }
 
+    /**
+     * Metodo para extraer y renderizar los proyectiles
+     * @param enemy Balas de los enemigos
+     * @param player Balas de los jugadores
+     * @param enemyBullet Image de las balas de los enemigos
+     * @param playerBullet Image de las balas del jugador
+     * @param gcontext GraphicsContext en el que se dibuja
+     */
     private void extractProjectiles(LinkedList<String> enemy, LinkedList<String> player, Image enemyBullet, Image playerBullet, GraphicsContext gcontext){
         if (((String) enemy.getElement(0).getDataT()).compareTo("") != 0) {
             for (Integer i = 0; i < enemy.getSize(); i++) {
@@ -157,6 +278,12 @@ public class ObserverScreen extends GameScreen{
         }
     }
 
+    /**
+     * Metodo para extraer y renderizar los bloques
+     * @param blocks Informacion de los bloques
+     * @param block Image de los bloques
+     * @param gcontext GraphicsContext en el que se dibuja
+     */
     private void extractBlocks(LinkedList<String> blocks, Image block, GraphicsContext gcontext){
         if (((String) blocks.getElement(0).getDataT()).compareTo("") != 0) {
             for (Integer i = 0; i < blocks.getSize(); i++) {
